8c6a1410feff1df57fb3c9606956d636d98223a9
SADICKY Dave <dsadicky@gmail.com>
2022-02-08 20:52:17 +0200

    modification de idprof par idcat

--- Model/Admin/articles.class.php
+++ Model/Admin/articles.class.php
@@ -111,17 +111,17 @@ Class Articles
         return $ok;
     }
 
-     public function activProf($idprof){
+     public function activProf($idcat){
          $db = getConnection();
          $sql =$db->prepare( "UPDATE prof SET ACCESS='1' where ID=?");
-         $ok = $sql->execute(array($idprof));
+         $ok = $sql->execute(array($idcat));
         return $ok;
      }
      
-    public function deactivProf($idprof){
+    public function deactivProf($idcat){
         $db = getConnection();
         $sql =$db->prepare( "UPDATE prof SET ACCESS='0' where ID=?");
-        $ok = $sql->execute(array($idprof));
+        $ok = $sql->execute(array($idcat));
         return $ok;
     }
 }
